55a05bbf0a3e490eb2eeb5f7f6359bdd331164fa
kinogam <kinogam@gmail.com>
2013-01-29 10:27:45 +0800

    features add

--- test/kino.placeholder-test.js
+++ test/kino.placeholder-test.js
@@ -5,15 +5,34 @@
 
 var TestSuit = {
     container: null,
-    usePlaceHolder: function () {
+    placeHolderInput: null,
+    init: function(){
         this.container = document.createElement("div");
-        this.container.innerHTML = "<input type='text' placeholder='hello world' />";
-
-        $(this.container).find('input').placeholder();
+        this.container.innerHTML = "<input type='text' placeholder='hello world' /><input type='text' />";
+        this.placeHolderInput = $(this.container).find("[placeholder]")[0]
+    },
+    getPlaceHolderInputValue: function () {
+        return this.placeHolderInput.value;
+    },
+    setPlaceHolderInputValue: function (val) {
+        $(this.placeHolderInput).val(val);
         return this;
     },
-    $el: function (index) {
-        return $(this.container).find('input:eq(' + index + ')');
+    focusOnPlaceHolderInput: function () {
+        $(this.placeHolderInput).trigger("focus");
+        return this;
+    },
+    blurOnPlaceHolderInput: function () {
+        $(this.placeHolderInput).trigger("blur");
+        return this;
+    },
+    afterUsePlaceHolder: function () {
+        $(this.container).find('input').placeholder();        
+        return this;
+    },
+    clearPlaceHolder: function () {
+        $(this.container).find('input').clearPlaceHolder();
+        return this;
     }
 };
 
@@ -27,6 +46,8 @@ module("placeholder", {
 
             }
         });
+
+        TestSuit.init();
     }
 });
 
@@ -34,34 +55,113 @@ module("placeholder", {
 test("如果浏览器原生支持元素placeholder属性,函数将不进行任何处理", function () {
     expect(0);
 
-    $.placeholderTestConfig({        
+    $.placeholderTestConfig({
         isNativeSupport: true,
         testFn: function () {
             ok(false);
         }
     });
 
-    TestSuit.usePlaceHolder();
+    TestSuit.afterUsePlaceHolder();
 
 });
 
 test("调用placeholder方法后，将会把placeholder的值显示到输入框", function () {
-    equal(TestSuit.usePlaceHolder().$el(0).val(), "hello world");
+    equal(TestSuit.afterUsePlaceHolder()
+        .getPlaceHolderInputValue(),
+        "hello world");
 });
 
 test("如果输入框原本内容为空，触发焦点后，输入框内的placeholder值将被清空", function () {
-    equal(TestSuit.usePlaceHolder().$el(0).trigger("focus").val(), "");
+    equal(TestSuit.afterUsePlaceHolder()
+        .focusOnPlaceHolderInput()
+        .getPlaceHolderInputValue(),
+        "");
 });
 
 test("如果输入框原本内容为空，离开焦点后，placeholder值将重新被填充", function () {
-    equal(TestSuit.usePlaceHolder().$el(0).trigger("focus").trigger("blur").val(), "hello world")
+    equal(TestSuit.afterUsePlaceHolder()
+        .focusOnPlaceHolderInput()
+        .blurOnPlaceHolderInput()
+        .getPlaceHolderInputValue()
+        , "hello world")
 });
 
 test("如果输入框原本内容不为空，离开焦点后，placeholder值将不会重新被填充", function () {
-    equal(TestSuit.usePlaceHolder().$el(0).trigger("focus").val("new text").trigger("blur").val(), "new text")
+    equal(TestSuit.afterUsePlaceHolder()
+        .focusOnPlaceHolderInput()
+        .setPlaceHolderInputValue("new text")
+        .blurOnPlaceHolderInput()
+        .getPlaceHolderInputValue(),
+        "new text")
 });
 
 test("如果输入框原本内容不为空，触发焦点后，输入框内的placeholder值将不会被清空", function () {
-    equal(TestSuit.usePlaceHolder().$el(0).trigger("focus").val("new text").trigger("blur").trigger("focus").val(), "new text");
+    equal(TestSuit.afterUsePlaceHolder()
+        .focusOnPlaceHolderInput()
+        .setPlaceHolderInputValue("new text")
+        .blurOnPlaceHolderInput()
+        .focusOnPlaceHolderInput()
+        .getPlaceHolderInputValue(),
+        "new text");
 });
 
+test("如果浏览器原生支持元素placeholder属性,clearPlaceHolder将不进行任何处理", function () {
+    expect(0);
+    $.placeholderTestConfig({
+        isNativeSupport: true,
+        testFn: function () {
+            ok(false);
+        }
+    });
+
+    TestSuit.afterUsePlaceHolder().clearPlaceHolder();
+});
+
+//clearPlaceHolder 只是执行 输入框的值的清理，不移除事件
+
+test("执行clearPlaceHolder操作后，如果输入框原本为空，则其值也为空", function () {
+    equal(TestSuit
+        .afterUsePlaceHolder()
+        .clearPlaceHolder()
+        .getPlaceHolderInputValue(),
+        "");
+});
+
+test("执行clearPlaceHolder操作后，如果输入框原本为不空，则其值不改变", function () {
+    equal(TestSuit
+        .afterUsePlaceHolder()
+        .focusOnPlaceHolderInput()
+        .setPlaceHolderInputValue("new text")
+        .blurOnPlaceHolderInput()
+        .clearPlaceHolder()
+        .getPlaceHolderInputValue(),
+        "new text");
+});
+
+test("如果使用placeHolder之前，输入框已经有值，则触发焦点之后，输入框保持原有值", function () {
+    equal(TestSuit
+       .setPlaceHolderInputValue("new text")
+       .afterUsePlaceHolder()
+       .focusOnPlaceHolderInput()
+       .getPlaceHolderInputValue(),
+       "new text");
+});
+
+test("如果使用placeHolder之后，输入框的值被其他脚本动态填充，则触发焦点之后，输入框保持原有值", function () {
+    equal(TestSuit
+   .afterUsePlaceHolder()
+   .focusOnPlaceHolderInput()
+   .setPlaceHolderInputValue("other text")
+   .getPlaceHolderInputValue(),
+   "other text");
+});
+
+test("如果使用placeHolder之后，输入框的值被其他脚本动态填充，则离开焦点之后，输入框保持原有值", function () {
+    equal(TestSuit
+   .afterUsePlaceHolder()
+   .setPlaceHolderInputValue("other text2")
+   .blurOnPlaceHolderInput()
+   .getPlaceHolderInputValue(),
+   "other text2");
+});
